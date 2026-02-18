583ee964562b17c3805bdd5113f0f3ec45ccbb5d
Lkarm003 <lkarm003@ucr.edu>
2026-02-18 12:32:57 -0800

    Add alternative chat message parsing implementation
    This code represents an alternative version of the initial code parsing logic, which utilizes a tree structure for parsing chat messages. The commented-out code provides a detailed implementation of the parsing process, including handling tags and regular text.

--- active-chat/active-chat-framework/src/parser/parsing_code.js
+++ active-chat/active-chat-framework/src/parser/parsing_code.js
@@ -85,3 +85,85 @@ class code_parsing
         return root;
     }
 }
+/*
+//This is the other version of the initial code parsing code that I created that works similar to the tree structure mentioned during discussion on 02/17/26. 
+parse(chat_message)
+{
+    let root = new ParsingTreeNode("root");
+    let stack = [root];
+
+    let regular_text = "";
+
+    for (let i = 0; i < chat_message.length; i++)
+    {
+        let currentNode = stack.at(-1);
+
+        if (chat_message[i] === '<')
+        {
+            if (regular_text.length > 0)
+            {
+                currentNode.addChildNode(
+                    new ParsingTreeNode("text", regular_text)
+                );
+                regular_text = "";
+            }
+
+            let index = chat_message.indexOf(">", i);
+
+            if (index === -1)
+            {
+                regular_text += "<";
+                continue;
+            }
+
+            let parsed_content =
+                chat_message.substring(i + 1, index).trim();
+
+            // validate tag
+            if (!/^\/?[a-zA-Z]/.test(parsed_content))
+            {
+                regular_text += "<";
+                continue;
+            }
+
+            // closing tag
+            if (parsed_content.startsWith("/"))
+            {
+                let tagName = parsed_content.slice(1);
+
+                while (stack.length > 1 &&
+                       stack.at(-1).value !== tagName)
+                {
+                    stack.pop();
+                }
+
+                if (stack.length > 1)
+                    stack.pop();
+            }
+            else
+            {
+                let tagNode =
+                    new ParsingTreeNode("tag", parsed_content);
+
+                currentNode.addChildNode(tagNode);
+                stack.push(tagNode);
+            }
+
+            i = index;
+        }
+        else
+        {
+            regular_text += chat_message[i];
+        }
+    }
+
+    if (regular_text.length > 0)
+    {
+        stack.at(-1).addChildNode(
+            new ParsingTreeNode("text", regular_text)
+        );
+    }
+
+    return root;
+}
+*/
