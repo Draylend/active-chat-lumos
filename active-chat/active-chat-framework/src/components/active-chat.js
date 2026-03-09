4a6e8537565ce806d8f66864496dc742f3c7a9e3
kshuang04 <shuan284@ucr.edu>
2026-03-09 14:23:04 -0700

    implemented gemini

--- active-chat/active-chat-framework/src/components/active-chat.js
+++ active-chat/active-chat-framework/src/components/active-chat.js
@@ -1,4 +1,7 @@
 // Class structure for active-chat
+import { GoogleGenAI } from "@google/genai";
+//const ai = new GoogleGenAI({apiKey: `${GEMINI_API_KEY}`});
+const ai = new GoogleGenAI({apiKey: `AIzaSyDFmSmMIUm95iMTLimxnz544plxRCmyB_w`});
 
 import { ChatMessage } from "./chat-message.js";
 import { parse } from "../parser/parsing_code.js";
@@ -180,18 +183,50 @@ class ActiveChat extends HTMLElement {
 
     // send user msg to llm
     async llmInteraction(prompt) {
-        const response = await fetch('http://localhost:11434/api/generate', {
-            method: 'POST',
-            body: JSON.stringify({
-                model: "ollama",
-                prompt: prompt,
-                stream: false
-            })
-        });
+        const response = await ai.models.generateContent({
+            model: "gemini-3-flash-preview",
+            contents: prompt,
+            config: {
+                systemInstruction: `
+You must follow these following rules:
 
-        const data = await response.json();
-        console.log(data.response);
-        parse(data.response);
+Rules:
+- All of your responses should be in markdown.
+- Be a conversational llm and be able to ask multiple choice questions.
+- When asked to give an mcq question, follow the given format below.
+- If you want to send text, send it as a separate message as the multiple choice question.
+- Don't start messages with headers, only text.
+- Do not include bold or italics in responses.
+- Before giving another question, give feedback on the answer first then give another question.
+- Give only one multiple choice question at a time.
+- Do not respond to messages that are chat interactions.
+- Do not default to "What is the capital of France" question.
+- Do not use emojis
+
+When asked to create a mcq:
+- Use the <chat-activity> tag
+- Inside, use component tags like <multiple-choice-question>
+- Use <question-header>, and <option-choice> tags
+- For the answer to a question, it must be an attribute of the <question-header> tag called "answer"
+- The answer to a question must be one of the answer options you provide
+- The answer to a question must be the exact text of the answer, so it matches the letter uppercase and lowercase, and no A, B, C, or D as the answer
+- Give 4 answer options for a question
+- Do not include numberings in the answer options
+- You must follow this formatting for an mcq question. Here is an example of an mcq question:
+<chat-activity>
+    <multiple-choice-question>
+        <question-header answer="10">What is 5 + 5?</question-header>
+            <option-choice>5</option-choice>
+            <option-choice>10</option-choice>
+            <option-choice>15</option-choice>
+    </multiple-choice-question>
+</chat-activity> 
+            `,
+            },
+        });
+    
+        console.log(response.text);
+        parse(response.text);
 
         // Auto-scroll to bottom when a new message is added
         this.chat.scrollTop = this.chat.scrollHeight;
